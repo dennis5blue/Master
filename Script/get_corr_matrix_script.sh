33645d9a2018e047776c83dc4af6ce59cd561214
dennis5blue <dennis5blue@gmail.com>
2015-05-12 16:07:46 +0800

    update corrMatrix generation

--- Script/get_corr_matrix_script.sh
+++ Script/get_corr_matrix_script.sh
@@ -1,45 +1,51 @@
-#mcc -R -nojvm -R -nodisplay -R -nodesktop -R -nosplash -mv -I ../SubME_1.6/ GenerateCorrMatrix.m
-for (( nn=1; nn<=20; nn=nn+1 ))
+#mcc -R -nojvm -R -nodisplay -R -nodesktop -R -nosplash -mv -I ../SubME_1.6/ GenerateCorrMatrixRegion.m
+for (( cc=1; cc<=1; cc=cc+1 ))
 do
-  for (( mm=1; mm<=20; mm=mm+1 ))
+  for (( dd=1; dd<=10; dd=dd+1 ))
   do
-    echo "
-    ### Job name
-    #PBS -N Prediction_"$nn"_"$mm"
+    for (( xx=1; xx<=16; xx=xx+1 ))
+    do
+      for (( yy=1; yy<=9; yy=yy+1 ))
+      	do
+    	echo "
+    	### Job name
+    	#PBS -N Prediction_"$xx"_"$yy"_"$cc"_"$dd"
 
-    ### out files
-    #PBS -e ./log/Prediction_"$nn"_"$mm".err
-    #PBS -o ./log/Prediction_"$nn"_"$mm".log
+    	### out files
+    	#PBS -e ./log/Prediction_"$xx"_"$yy"_"$cc"_"$dd".err
+    	#PBS -o ./log/Prediction_"$xx"_"$yy"_"$cc"_"$dd".log
 
-    ### put the job to which queue (qwork)
-    #PBS -q qwork
-    " > ./temp/Prediction_"$nn"_"$mm".sh
+    	### put the job to which queue (qwork)
+    	#PBS -q qwork
+    	" > ./temp/Prediction_"$xx"_"$yy"_"$cc"_"$dd".sh
 
-    echo '
-    # show the time and information
-    echo Working directory is $PBS_O_WORKDIR
-    cd $PBS_O_WORKDIR
-    echo Running on host `hostname`
-    echo Start time is `date`
-    time1=`date +%s`
-    echo Directory is `pwd`
-    ' >> ./temp/Prediction_"$nn"_"$mm".sh
+    	echo '
+    	# show the time and information
+    	echo Working directory is $PBS_O_WORKDIR
+    	cd $PBS_O_WORKDIR
+    	echo Running on host `hostname`
+    	echo Start time is `date`
+    	time1=`date +%s`
+    	echo Directory is `pwd`
+    	' >> ./temp/Prediction_"$xx"_"$yy"_"$cc"_"$dd".sh
 
-    echo "
-    # run the script
-    MCR_tmp=\"/tmp/mcr_\$RANDOM\"
-    mkdir \$MCR_tmp
-    export MCR_CACHE_ROOT=\$MCR_tmp
-    ./GenerateCorrMatrix "$nn" "$mm"
-    rm -rf \$MCR_tmp						
-    " >>  ./temp/Prediction_"$nn"_"$mm".sh
+    	echo "
+    	# run the script
+    	MCR_tmp=\"/tmp/mcr_\$RANDOM\"
+    	mkdir \$MCR_tmp
+    	export MCR_CACHE_ROOT=\$MCR_tmp
+    	./GenerateCorrMatrixRegion "$xx" "$yy" "$cc" "$dd"
+    	rm -rf \$MCR_tmp						
+    	" >>  ./temp/Prediction_"$xx"_"$yy"_"$cc"_"$dd".sh
 
-    echo '
-    echo End time is `date`
-    time2=`date +%s`
-    echo Computing time is `echo $time2-$time1 | bc` sec
-    ' >>   ./temp/Prediction_"$nn"_"$mm".sh
+    	echo '
+    	echo End time is `date`
+    	time2=`date +%s`
+    	echo Computing time is `echo $time2-$time1 | bc` sec
+    	' >>   ./temp/Prediction_"$xx"_"$yy"_"$cc"_"$dd".sh
 
-    qsub ./temp/Prediction_"$nn"_"$mm".sh
+    	qsub ./temp/Prediction_"$xx"_"$yy"_"$cc"_"$dd".sh
+      done
+    done
   done
 done

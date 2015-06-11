0e4e176c987cd00d4ded60d8c08f30ace3870099
dennisyu <dennis5blue@gmail.com>
2015-06-11 15:13:11 +0800

    use jmvc to get input data

---
+++ SourceData/test8/encode_phase0.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python2.7
+
+from subprocess import Popen, PIPE
+from subprocess import call
+import re
+import sys
+
+encoderPath = "/home/dennisyu/Documents/JMVC/bin/H264AVCEncoderLibTestStaticd"
+configurePath = "./cfg/"
+yuvPath = "./yuv/"
+outputPath = "./outFiles/"
+
+# Independent coding
+def idtCoding(processList, outFile):
+    output = ""
+    for i in range(len(processList)):
+        cam = "camera_"+str(i+1)+".yuv"
+        call(["cp",yuvPath+cam, "./jmvcTempFiles/tmp_0.yuv"])
+	print(yuvPath+cam)
+	output = Popen([encoderPath,"-vf",configurePath+"01Ex4Idt.cfg","0"],stdout=PIPE).communicate()[0]
+	print (output)        
+	print "encode: ", i
+	lines = [line for line in output.split('\n') if "byte" in line]
+	byteInfo = " ".join(lines[0].strip().replace("[","").replace("]","").split())
+	outFile.write(" Code: "+str(i)+" "+byteInfo+"\n")
+
+def main(argv = None): 
+    output = Popen(["ls", yuvPath],stdout=PIPE).communicate()[0] 
+    fileList = output.split('\n')
+    fileList = filter(None,fileList)
+    #print (fileList)
+    call(["rm","./jmvcTempFiles/*.yuv","./jmvcTempFiles/*.264"])
+    outFile = open(outputPath+"phase_"+str(0)+".txt","w+")
+    result = idtCoding(fileList, outFile)
+        
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ SourceData/test8/yuv_gen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python2.7
+
+from subprocess import Popen, PIPE
+from subprocess import call
+import re
+import sys
+
+def main(argv=None):
+	pngSeqPath = "./png/"
+	yuvSeqPath = "./yuv/"
+	
+	output = Popen(["ls", pngSeqPath],stdout=PIPE).communicate()[0] 
+	fileList = output.split('\n')
+	fileList = filter(None,fileList)
+ 
+	processList = len(fileList)*[None] 
+	for i in range(len(fileList)):
+		idx = int(re.sub(r'camera_|\.png','',fileList[i]))-1 # minus 1 since python index start at 0
+		processList[idx] = re.sub(r'\.png','',fileList[i])
+		#print (processList[idx])
+		#print (len(processList))
+
+	for i in range(len(fileList)):
+        	print pngSeqPath+processList[i]+".png"
+        	print yuvSeqPath+processList[i]+".yuv"
+        	call(["../../../ffmpeg","-i", pngSeqPath+processList[i]+".png","temp.mp4"])
+        	call(["../../../ffmpeg","-i", "temp.mp4",yuvSeqPath+processList[i]+".yuv"])
+        	call(["rm","temp.mp4"])
+
+if __name__ == "__main__":
+	sys.exit(main())

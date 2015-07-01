6d0c802311540b2d7aef48da4de09b600c1def62
dennis5blue <dennis5blue@gmail.com>
2015-07-01 23:10:42 +0800

    update thesis

--- imageTest/plotBaisSg.m
+++ imageTest/plotBaisSg.m
@@ -2,36 +2,38 @@ clc;
 clear;
 
 figure(1);
-indep = 90447;
-fullsearch256 = [(indep-68727)/indep 0 0 0 0];
-fullsearch128 = [0 (indep-73762)/indep 0 0 0];
-%bais32 = [(73346-indep)/indep]
-bais64 = [0 0 (indep-70866)/indep 0 0];
+indep = 57766;
+%fullsearch256 = [(indep-68727)/indep 0 0 0 0];
+%fullsearch128 = [0 (indep-73762)/indep 0 0 0];
+bais0 = [(indep-57030)/indep 0 0 0 0];
+bais100 = [0 (indep-37068)/indep 0 0 0];
 %bais96 = [(70148-indep)/indep]
-bais128 = [0 0 0 (indep-69636)/indep 0];
+bais200 = [0 0 (indep-35008)/indep 0 0];
 %bais160 = [(84515-indep)/indep]
-bais192 = [0 0 0 0 (indep-85879)/indep];
+bais300 = [0 0 0 (indep-34348)/indep 0];
+bais400 = [0 0 0 0 (indep-56874)/indep];
 
-x = [1:length(fullsearch128)];
-bar(x, fullsearch256, 'FaceColor', [1 0.5 0], 'DisplayName','Full search (range = 256)'); hold on;
-bar(x, fullsearch128, 'FaceColor', [0 0.5 0.2], 'DisplayName','Full search (range = 128)'); hold on;
-bar(x, bais64, 'FaceColor', [0 0.5 0.4], 'DisplayName','Baised full search (range = 128, baised pixels = 64)'); hold on;
-bar(x, bais128, 'FaceColor', [0 0.5 0.6], 'DisplayName','Baised full search (range = 128, baised pixels = 128)'); hold on;
-bar(x, bais192, 'FaceColor', [0 0.5 0.8], 'DisplayName','Baised full search (range = 128, baised pixels = 192)');
+x = [1:length(bais0)];
+bar(x, 100.*bais0, 'FaceColor', [1 0.5 0], 'DisplayName','Conventional motion estimation'); hold on;
+bar(x, 100.*bais100, 'FaceColor', [0 0.5 0.2], 'DisplayName','Biased pixels = 100'); hold on;
+bar(x, 100.*bais200, 'FaceColor', [0 0.5 0.4], 'DisplayName','Biased pixels = 200'); hold on;
+bar(x, 100.*bais300, 'FaceColor', [0 0.5 0.6], 'DisplayName','Biased pixels = 300'); hold on;
+bar(x, 100.*bais400, 'FaceColor', [0 0.5 0.8], 'DisplayName','Biased pixels = 400');
 
-leg = legend('show','location','NorthEast');
+leg = legend('show','location','NorthWest');
 set(leg,'FontSize',11);
-axis([-inf inf 0 0.35]);
+axis([-inf inf 0 60]);
 set(gca,'XTick',[]);
 %set(gca,'YTick',[]);
 %set(gca,'YColor','w');
 %set(gca,'XColor','w');
 set(gca,'XTickLabel',['']);
 %set(gca,'YTickLabel',['']);
-ylabel('Reduction ratio (%)');
+ylabel('Reduced encoded bits (%)','FontSize',11);
 xlabel('');
 grid on;
 
+%{
 figure(2);
 fullsearch256Time = [49*60+51 0 0 0 0];
 fullsearch128Time = [0 14*60+44 0 0 0];
@@ -61,4 +63,4 @@ set(gca,'XTickLabel',['']);
 ylabel('Encoded time (sec)');
 xlabel('');
 grid on;
-
+%}
